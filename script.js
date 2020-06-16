dff9a32d0f7c9bb72a9ee6b7441eb4fefa795acc
Maya Stucky <mayaatstucky@gmail.com>
2020-06-16 08:09:18 -0400

    appended information for weather information for hard coded city

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
   console.log("JavaScript LINKED!");
 
-  //   var city = $("#userCity").val();
-  var city = "Atlanta";
+  var city = "Brunswick";
+  var dateToday = moment().format("L");
   var APIkey = "6340db1bd8528765d6e74f5851c1d26a";
   var queryURL =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     city +
-    "&appid=" +
+    "&units=imperial&appid=" +
     APIkey;
 
   $.ajax({
@@ -15,6 +15,23 @@ $(document).ready(function () {
     method: "GET",
   }).then(function (response) {
     console.log(response);
+
+    //render the searched city AND the current date
+    var searchedCity = $("#current-city").text(response.name + " " + dateToday);
+    console.log(searchedCity);
+    //now we need to get the information for that city
+    //temperature
+    var cityTemp = $("#temperature").text(response.main.temp);
+    console.log(response.main.temp);
+    //humidity
+    var cityHumidity = $("#humidity").text(response.main.humidity + "%");
+    console.log(response.main.humidity);
+    //wind
+    var cityWind = $("#wind").text(response.wind.speed + " MPH");
+    console.log(response.wind.speed);
+
+    //NEED SEPARATE API CALL FOR UV RAYS!
+    // var cityUV = $("#uv").text(response.)
   });
 });
 
